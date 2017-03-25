24300c01210433c64a6cc55bd873075dfc8b2211
chizhang <zhang.chi1@husky.neu.edu>
2017-03-25 17:40:30 -0400

    added mainmenu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public string levelToLoad = "MainScene";
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//play the game
+	public void Play() {
+		SceneManager.LoadScene (levelToLoad);
+
+	}
+
+	//quit the game
+	public void Quit ()
+	{
+		Debug.Log ("Quit the game!");
+		Application.Quit ();
+	}
+}
