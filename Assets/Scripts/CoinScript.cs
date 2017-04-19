2ec93cf303565a98707f42c232dd3a6a19bcd412
Alec Jordan <alecbjordan@gmail.com>
2017-04-18 22:40:57 -0400

    Added coin and axe (/w scripts and models)

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+	
+	void Update () {
+		transform.Rotate(0, 140 * Time.deltaTime, 0);
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/SwingScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingScript : MonoBehaviour {
+
+	float angle = 0;
+
+	void Update () {
+		//float rot = Mathf.Sin ( Time.deltaTime);
+		angle = 180 + 90 * Mathf.Sin (Time.time * 1.5f);
+		transform.rotation = Quaternion.Euler( 0, 0, angle);
+		//transform.eulerAngles = Vector3(0, 0, 90 * Mathf.Sin(Time.deltaTime));
+	}
+}
+	
+
+
